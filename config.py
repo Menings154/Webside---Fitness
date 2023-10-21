e3e6c6abbc6300f91cb4e00ab94bf2b90d982e9e
Menings <menings.git@gmail.com>
2023-10-21 10:33:19 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # ich will hier später true, da das ein signal an die app sendet, wenn eine änderung passiert
