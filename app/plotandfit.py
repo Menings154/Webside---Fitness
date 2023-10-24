b6d853d3c1d73c2a822fcd203dfa61135a7de899
Menings <menings.git@gmail.com>
2023-10-24 06:34:51 +0200

    first idea of plotandfit module

---
+++ app/plotandfit.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def plot_data(x, y, fig, ax):
+    "Plot x and y data in a nice format"
+    pass
+
+def linear_fit(x, y, fig, ax):
+    "Make a linear fit and plot the result into the given axes."
+    pass
+
+def make_date_label(dates, fig, ax):
+    "Make the label for a date-axis"
+    pass
